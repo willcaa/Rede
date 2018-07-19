976ac21919cf4985ded67b3ff932259367bfb51b
Hugo Paulista <hugoarpaulista@gmail.com>
2018-07-19 19:55:01 -0300

    19/07

--- src/pages/codigo-de-erro/codigo-de-erro.ts
+++ src/pages/codigo-de-erro/codigo-de-erro.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Http, Headers, RequestOptions } from '@angular/http';
 import { MarcasArCondicionadoPage } from '../marcas-ar-condicionado/marcas-ar-condicionado';
-
+import { AlertController } from 'ionic-angular';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+import { text } from '@angular/core/src/render3/instructions';
 /**
  * Generated class for the CodigoDeErroPage page.
  *
@@ -17,14 +19,31 @@ import { MarcasArCondicionadoPage } from '../marcas-ar-condicionado/marcas-ar-co
 })
 export class CodigoDeErroPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, public alertCtrl: AlertController) {
   }
   goPage(){
     this.navCtrl.push(MarcasArCondicionadoPage);
   }
 
+  new(){
+    let confirm = this.alertCtrl.create({
+      title: "Novidade!",
+      message: "Uma nova ferramenta Código de Erro está sendo desenvolvida. Mais novidades em breve!",
+      buttons:[
+        {
+          text: "Ok",
+          handler: () => {
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad CodigoDeErroPage');
+    this.new();
+
   }
 
 }
